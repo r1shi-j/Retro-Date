1505fa41fedad2f043c24c1667383f466018b0cf
Rishi <rishi_j@icloud.com>
2025-08-24 10:57:23 +0100

    Initial Commit

---
+++ Retro Date/Retro_DateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Retro_DateApp.swift
+//  Retro Date
+//
+//  Created by Rishi Jansari on 24/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct Retro_DateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
